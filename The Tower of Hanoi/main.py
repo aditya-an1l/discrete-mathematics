b14968cba836af7e819a0b282e0b245268839fe6
aditya-an1l <aditya.anil.productions@gmail.com>
2024-09-04 22:13:45 +0530

    Add code for the puzzle The tower of Hanoi
    - The program computes the movement that must be made to solve the
    puzzle in text form.

---
+++ The Tower of Hanoi/main.py
@@ -0,0 +1,22 @@
+"""
+Source: https://www.geeksforgeeks.org/python-program-for-tower-of-hanoi/
+Contributed by Dilip Jain to geeksforgeeks
+Time Complexity: O(2^n)
+Auxiliary Space: O(n)
+"""
+
+
+# Recursive Python function to solve the tower of hanoi
+def TowerOfHanoi(n, source, destination, auxiliary):
+    if n == 1:
+        print("Move disk 1 from source", source, "to destination", destination)
+        return
+    TowerOfHanoi(n - 1, source, auxiliary, destination)
+    print("Move disk", n, "from source", source, "to destination", destination)
+    TowerOfHanoi(n - 1, auxiliary, destination, source)
+
+
+number_of_discs = 4
+
+TowerOfHanoi(number_of_discs, "A", "B", "C")
+# A, C, B are the name of rods
